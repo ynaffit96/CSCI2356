fa45572c86b3b6006a83cb83fdb0ef1838c1d64d
Tiffany Conrad <tiffany.conrad@smu.ca>
2021-04-09 01:03:45 -0300

    DO NOT COMMIT! Look for my mistake :)

--- P3/scripts/P3Client.js
+++ P3/scripts/P3Client.js
@@ -4,6 +4,8 @@
 @Author Tahira Tabassum (A00416670)
 */
 
+var SERVER_URL = "http://ugdev.cs.smu.ca:3919";
+
 function init() {
   drawLogo();
   $("#windowSlider").on("change", function () {
@@ -14,7 +16,7 @@ function init() {
     processInput();
   });
 
-  $("#opaqueThick").on("change",function(){
+  $("#opaqueThick").on("change", function () {
     processInput();
   });
 
@@ -43,10 +45,9 @@ function init() {
   $("#kWh").on("change", function () {
     processInput();
   });
-  $("#chapters").on("change",function(){
+  $("#chapters").on("change", function () {
     processInput();
   });
-
 
   let construction = $("#opaqueThick").val();
   let window = $("#windowSlider").val();
@@ -87,7 +88,7 @@ function clearBottom() {
   let choice = $("#chapters").find(":selected").text();
   let page = document.getElementById("clearedArea");
 
-  if (choice == "VIEW CHAPTERS") {
+  if (choice != "Insulation") {
     page.hidden = true;
   } else {
     page.hidden = false;
@@ -147,16 +148,12 @@ function processInput() {
   let construction = $("#opaqueThick").val();
   let constructionType = $("#insulationOptions option:selected").val();
   let window = $("#windowSlider").val();
-  let chapters = $("#chapters").val()
-  
+  let chapters = $("#chapters").val();
 
   draw(construction, window);
   opaqueThick(construction, constructionType);
   calculateOutputBoxes(construction, window);
-  concept();
-
-  
-
+  alertChapters();
 }
 
 /*
@@ -447,66 +444,53 @@ function doorThermalResistanceOutput() {
 
 /**
  *  This function writes the concepts, depending on the user's choice from the Concepts dropdown menu
+ *  Receives information from Server (P3Server.js)
  */
-function concept() {
+function myRoute() {
   let concept = $("#concepts").val();
   let chapters = $("#chapters").val();
 
-  if(chapters == "top"){
+  if (chapters == "top") {
     $("#text").html(" ");
-  } else if(chapters == "2"){
-  if (concept === "1") {
-    $("#text").html(
-      "Local Conditions: </br></br> Heating demand is given in heating degree-days. " +
-        "The length of a Canadian heating season is the number of days below 18&#8451. Coldness is the difference between a" +
-        " desired indoor temperature of 20&#8451 and the average outdoor temperature on those days </br></br>" +
-        "Humidity and especially windiness of a location also add to heating demand but are discussed elsewhere.</br></br>" +
-        "Warmer climates imply a cooling load: also a subject for other chapters.</br></br>" +
-        "Please note that to reflect the Canadian experience, this App mixes units: Celsius for temperature, for example, but inches for dimensions"
-    );
-  } else if (concept === "2") {
-    $("#text").html(
-      "Annual Energy Budget:</br></br>Envelope heat loss is only part of an energy budget. Lights, hot water appliances " +
-        "and electronics also consume energy. In this chapter those other loads are fixed, on the assumption that use of the building remains " +
-        "constant in all locations.</br></br>Envelope heat loss has at least two components: the effectively conductive losses that can be " +
-        "reduced by insulation, and lossed due to ventilation and drafts. Both are proportional to heat demand. Looking at the Energy Budget" +
-        " Graph, you will see that changing the insulation levels changes the conductive or insulation losses but not those due to air movement."
-    );
-  } else if (concept === "3") {
-    $("#text").html(
-      "Drafts and Ventilation:</br></br>Realistically, a larger window would admit more drafts, especially at the lower end of " +
-        "the quality scale, but that effect is ignored in the insulation chapter.</br></br>The Drafts and Ventilation chapter explains how energy" +
-        " losses due to infiltration are controlled by membranes, sealants, joint design, and meticulous quality control. It shows how" +
-        " ventilation losses can be managed through heat exchange, flow controls, and careful use of operable windows and vents."
-    );
-  } else if (concept === "4") {
-    $("#text").html(
-      "Insulation and Heat Loss:</br></br>In North America, thermal resistance is measured in R-Values. The resistance" +
-        " of a material barrier is a product of its resistivity, in R/inch, and the inches of thickness. The actual effectiveness of insulation " +
-        "depends on other factors, but this app gives drywall an R/inch of 1, fiberglass and cellulose insulation an R/inch of 3, and urethane " +
-        "spray foam an R/inch of 6.</br></br>In thin and poorly insulating assemblies, air films become significant. This is how painted sheet " +
-        "steel ends up with a nominal R of 1. When assemblies are layered, R values can simply be totalled."
-    );
-  } else if (concept === "5") {
-    $("#text").html(
-      "Materials and Insulation</br></br>Heat flow is inversely related to thermal resistance. The conduction of heat through " +
-        "a material is given as a U value, which is equal to 1/R. Add layers into a single R value before finding their U value.</br></br>" +
-        "Heat loss is a product of thermal demand and conductive liability. Thermal demand consolidates temperature differemce and time, as " +
-        "in degree days. Thermal liability is a product of surface area and conductance</br></br>The total thermal liability of an envelope " +
-        "is a sum of the liability of its portions. Average conductance divides the total liability by the total area. The effective R-value " +
-        "of an envelope is the inverse of average conductance.</br></br>Note that high R-value portions of an envelope have a smaller effect on " +
-        "the effective R-value than might be supposed. Conversely, low-R-value portions of an envelope such as windows have a larger effect on over" +
-        "all heat loss than their small area may suggest."
-    );
-  } else if (concept === "6") {
-    $("#text").html(
-      "Environmental Impact</br></br>The environmental impact of construction depends not only on the energy consumed in operating " +
-        "a building, but in the energy consumed or 'embodied' in the material through sourcing, manufacture, transport, and assembly. Additionally, tox" +
-        "ins and other ecological and social injuries need to be accounted for. The exact calculations are complicated and debatable, but that's no reas" +
-        "on to ignore them. They are the subject of several other chapters."
-    );
+  } else if (chapters == "2") {
+    if (concept === "1") {
+      $.get(SERVER_URL + "/myroute",function(data){
+        $("#text").html(data.localConditions);
+      }).fail(function(error){
+        alert(error.responseText);
+      });
+    } else if (concept === "2") {
+      $.get(SERVER_URL + "/myroute",function(data){
+        $("#text").html(data.annualEnergy);
+      }).fail(function(error){
+        alert(error.responseText);
+      });
+    } else if (concept === "3") {
+      $.get(SERVER_URL + "/myroute",function(data){
+        $("#text").html(data.draftsVent);
+      }).fail(function(error){
+        alert(error.responseText);
+      });
+    } else if (concept === "4") {
+      $.get(SERVER_URL + "/myroute",function(data){
+        $("#text").html(data.insulationHeatLoss);
+      }).fail(function(error){
+        alert(error.responseText);
+      });
+    } else if (concept === "5") {
+      $.get(SERVER_URL + "/myroute",function(data){
+        $("#text").html(data.materialInsul);
+      }).fail(function(error){
+        alert(error.responseText);
+      });
+    } else if (concept === "6") {
+      $.get(SERVER_URL + "/myroute",function(data){
+        $("#text").html(data.enviroImpact);
+      }).fail(function(error){
+        alert(error.responseText);
+      });
+    }
   }
-}
 }
 
 /***
@@ -580,7 +564,7 @@ function calculateOutputBoxes(construction, window) {
   let opaqueConstructionWithR = $("#insulationOptions").val();
 
   let windowThermalResistance = $("#windowThermal").val();
-  let doorThermalResistance = $("#doorSliderReadout").val(); 
+  let doorThermalResistance = $("#doorSliderReadout").val();
 
   // prints window area value in readoutbox
   $("#winSlidOut").val(windowAreaTrunc);
@@ -594,7 +578,7 @@ function calculateOutputBoxes(construction, window) {
       windowThermalResistance,
       doorThermalResistance
     )
-  );  
+  );
 
   $("#kWh").val(annualEnergyOutput());
 }
@@ -660,13 +644,37 @@ function annualEnergyOutput() {
   let degrees = $("#degrees").val();
   let opaqueThickness = $("#opaqueThickness").val();
 
-  if (
-    windowThermal >= 1 &&
-    doorThermal >= 2 &&
-    $("#degrees").val() != "top"
-  ) {
-    return Math.trunc((820 * degrees * 1.8 * 24 / effectiveOverallThermalResistanceOutput($("#opaqueThermal").val(),windowThermal,doorThermal))/3412+degrees*1.8*24*65/3412+3000);
+  if (windowThermal >= 1 && doorThermal >= 2 && $("#degrees").val() != "top") {
+    return Math.trunc(
+      (820 * degrees * 1.8 * 24) /
+        effectiveOverallThermalResistanceOutput(
+          $("#opaqueThermal").val(),
+          windowThermal,
+          doorThermal
+        ) /
+        3412 +
+        (degrees * 1.8 * 24 * 65) / 3412 +
+        3000
+    );
   } else {
     return "";
+  }
+}
+
+/***
+ * This function sends an alert to the user when a greyed out option is selected from Chapters
+ * 
+ * @author Tiffany Conrad (a00414194)
+ */
+function alertChapters() {
+  var chapter = $("#chapters").val();
+  var chap = $("#chapters option:selected").prop('label');
+  if (chapter != 2 && chapter != "top") {
+    chapter = "top";
+    if(alert(chap + " is under construction")){
+
+    }
+    window.location.reload();
+    init();
   }
 }
